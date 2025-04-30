733d41e3aa3c3baa78d85bb9da7d73b54d8665a3
unknown <kodms08@naver.com>
2025-05-01 00:24:24 +0900

    🎉init: 프로젝트 생성

---
+++ woorepie/src/main/java/com/piehouse/woorepie/WoorepieApplication.java
@@ -0,0 +1,13 @@
+package com.piehouse.woorepie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorepieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoorepieApplication.class, args);
+    }
+
+}
---
+++ woorepie/src/test/java/com/piehouse/woorepie/WoorepieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piehouse.woorepie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorepieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
