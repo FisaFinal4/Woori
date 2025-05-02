9b478e82bc22b6e623449eff6353ba1a2d08ddd9
letmeloveyou82 <shineanl04@gmail.com>
2025-05-02 10:35:08 +0900

    ⚡feat: ApiResponse에 JsonInclude 적용하여 null 필드 응답에서 제외

--- woorepie/src/main/java/com/piehouse/woorepie/global/response/ApiResponse.java
+++ woorepie/src/main/java/com/piehouse/woorepie/global/response/ApiResponse.java
@@ -1,7 +1,10 @@
 package com.piehouse.woorepie.global.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
         LocalDateTime timestamp,
         int status,
