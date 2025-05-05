089ca4b91955bc5ce7f2988629d607d89169e52b
Aunsxm <tngus893716@gmail.com>
2025-05-05 11:36:50 +0900

    ⚡feat: 매도 플로우 코드 추가(RedisRequest dto 추가/ config파일 수정)

---
+++ woorepie/src/main/java/com/piehouse/woorepie/trade/dto/request/RedisEstateTradeValue.java
@@ -0,0 +1,15 @@
+package com.piehouse.woorepie.trade.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RedisEstateTradeValue {
+    private Long customerId;
+    private int tokenAmount;
+    private int tokenPrice;
+    private long timestamp;
+}
