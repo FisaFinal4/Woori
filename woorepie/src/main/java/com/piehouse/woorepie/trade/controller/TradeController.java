361e939a8a37bdff34a43bfcfad2208f1342b423
Aunsxm <tngus893716@gmail.com>
2025-05-07 15:05:33 +0900

    ✏️fix : TradeController 수정

--- woorepie/src/main/java/com/piehouse/woorepie/trade/controller/TradeController.java
+++ woorepie/src/main/java/com/piehouse/woorepie/trade/controller/TradeController.java
@@ -1,10 +1,12 @@
 package com.piehouse.woorepie.trade.controller;
 
+import com.piehouse.woorepie.customer.dto.SessionCustomer;
 import com.piehouse.woorepie.trade.dto.request.BuyEstateRequest;
 import com.piehouse.woorepie.trade.dto.request.SellEstateRequest;
 import com.piehouse.woorepie.trade.service.TradeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,15 +17,17 @@ public class TradeController {
     private final TradeService tradeService;
 
     @PostMapping("/buy")
-    public ResponseEntity<String> buy(@RequestBody BuyEstateRequest request) {
-        Long customerId = 123L;  // TODO: 로그인 사용자 정보로 교체 예정
+    public ResponseEntity<String> buy(@RequestBody BuyEstateRequest request,
+                                      @AuthenticationPrincipal SessionCustomer sessionCustomer) {
+        Long customerId = sessionCustomer.getCustomerId();
         tradeService.buy(request, customerId);
         return ResponseEntity.ok("매수 요청 완료");
     }
 
     @PostMapping("/sell")
-    public ResponseEntity<String> sell(@RequestBody SellEstateRequest request) {
-        Long customerId = 123L;
+    public ResponseEntity<String> sell(@RequestBody SellEstateRequest request,
+                                       @AuthenticationPrincipal SessionCustomer sessionCustomer) {
+        Long customerId = sessionCustomer.getCustomerId();
         tradeService.sell(request, customerId);
         return ResponseEntity.ok("매도 요청 완료");
     }
